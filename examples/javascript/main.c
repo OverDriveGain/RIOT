654dc1a3fe2087f11669301a847af9af9947e60b
manar <manar.zaboub@gmail.com>
2018-10-21 19:49:02 +0200

    Add coap listener register program

--- examples/javascript/main.c
+++ examples/javascript/main.c
@@ -20,7 +20,6 @@
  * @}
  */
 
-#include "shell.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -32,50 +31,11 @@
 /* include headers generated from *.js */
 #include "lib.js.h"
 #include "local.js.h"
-
-// MZTODO REBASE VARIABLE
-int js_run_local(const jerry_char_t *script, size_t script_size)
-{
-
-    jerry_value_t parsed_code, ret_value;
-    int res = 0;
-
-    /* Initialize engine, no flags, default configuration */
-
-    jerry_init(JERRY_INIT_EMPTY);
-
-    /* Register the print function in the global object. */
-
-    jerryx_handler_register_global((const jerry_char_t *) "print",
-                                   jerryx_handler_print);
-
-    /* Setup Global scope code */
-
-    parsed_code = jerry_parse(NULL, 0, script, script_size, JERRY_PARSE_NO_OPTS);
-
-    if (!jerry_value_is_error(parsed_code)) {
-        /* Execute the parsed source code in the Global scope */
-        ret_value = jerry_run(parsed_code);
-        if (jerry_value_is_error(ret_value)) {
-            printf("js_run(): Script Error!");
-            res = -1;
-        }
-        jerry_release_value(ret_value);
-    }
-
-    jerry_release_value(parsed_code);
-
-    /* Cleanup engine */
-    jerry_cleanup();
-
-    return res;
-}
+#include "main.js.h"
 
 static event_queue_t event_queue;
 
-
 char script[2048];
-
 
 #define MAIN_QUEUE_SIZE (4)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
@@ -89,16 +49,20 @@ void js_start(event_t *unused)
 
     size_t script_len = strlen(script);
     if (script_len) {
-        puts("(re)initializing jerryscript engine...");
+        puts("Initializing jerryscript engine...");
         js_init();
-        js_run_local(lib_js, lib_js_len);
-        js_run_local(local_js, local_js_len);
+
+        puts("Executing lib.js...");
+        js_run(lib_js, lib_js_len);
+
+        puts("Executing local.js...");
+        js_run(local_js, local_js_len);
 
         puts("Executing script...");
-        js_run_local((jerry_char_t*)script, script_len);
+        js_run((jerry_char_t*)script, script_len);
     }
     else {
-        puts("Emtpy script, nothing to execute yet.");
+        puts("Emtpy script, not executing.");
     }
 }
 
@@ -109,17 +73,14 @@ void js_restart(void)
     js_shutdown(&js_start_event);
 }
 
-
-
-
-
-//int starter(int argc, char **argv)
-int starter(void)
+int main(void)
 {
+    printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
+    printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
-//    printf("Printing unused valuse :) %i %s" , argc, argv[0]);
+
     puts("waiting for network config");
-        
     xtimer_sleep(3);
 
     /* print network addresses */
@@ -135,24 +96,18 @@ int starter(void)
     event_queue_init(&event_queue);
     js_event_queue = &event_queue;
 
+    puts("Initializing jerryscript...");
+    js_event_queue = &event_queue;
+    js_init();
+
+    puts("Executing lib.js...");
+    js_run(lib_js, lib_js_len);
+
+    puts("Executing main.js...");
+    js_run(main_js, main_js_len);
+
     puts("Entering event loop...");
     event_loop(&event_queue);
-    return 0 ;
-}
 
-
-//static const shell_command_t commands[] = {
-//    { "w", "command description", starter },
-//    { NULL, NULL, NULL }
-//};
-int main(void)
-{
-    printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
-    printf("This board features a(n) %s MCU.\n", RIOT_MCU);
-    char target[]= "this is our target array, we wanna read this."; 
-//    char line_buf[SHELL_DEFAULT_BUFSIZE];
-    starter();
-    //   shell_run(commands, line_buf, SHELL_DEFAULT_BUFSIZE);
-    printf("%s\n", target);
-    return 0;    
+    return 0;
 }
